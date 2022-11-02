16b7130335687b49e499b6a6b51dd8177e3d4cb5
ggcrunchy <steve@xibalbastudios.com>
2022-11-02 14:51:46 -0500

    First go at moonassimp (sans tvOS at the moment)

---
+++ plugins/2016.2874/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_libmoonassimp' },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
